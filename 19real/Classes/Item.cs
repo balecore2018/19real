e8e7990ba3b4f21654ac79151338871e89010a24
dima <balecored@gmail.com>
2026-03-10 05:18:37 +0500

    Разработанный класс. Шаповалов

---
+++ 19real/Classes/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19real.Classes
+{
+    internal class Item
+    {
+        public string name {  get; set; }
+        public int price { get; set; }
+        public string src { get; set; }
+        public Item(string name, int price, string src)
+        {
+            this.name = name;
+            this.price = price;
+            this.src = src;
+        }
+    }
+}
